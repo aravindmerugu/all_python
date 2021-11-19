cef9204263cb20bf60654f4e0ce983aedd83e149
Aravind Merugu <32273334+aravindmerugu@users.noreply.github.com>
2021-11-19 11:19:21 +0000

    Every Day Mail

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import yagmail
 import os
 import time
+from datetime import datetime as dt
 
+
+sender = "aravind.merugu2610@gmail.com"
+receiver = "aravindmeruguwgl@gmail.com"
+subject = "EveryDay Mail"
+contents = """
+sending mail to multiple users.
+"""
 while(True):
-  sender = "aravind.merugu2610@gmail.com"
-  receiver = "aravindmeruguwgl@gmail.com"
-  subject = "Periodic Mail"
-  contents = """
-  sending mail to multiple users.
-  """
-
-  yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD'))
-  yag.send(to=receiver,subject=subject,contents=contents)
-  print('Email Sent')
-  time.sleep(60.0)
+  now = dt.now()
+  if now.hour == 10 and now.minute == 25:
+    yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD'))
+    yag.send(to=receiver,subject=subject,contents=contents)
+    print('Email Sent')
+    time.sleep(60.0)
